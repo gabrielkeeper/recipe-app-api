5c09788065713052018015dde1327ed261a02b87
gabrielkeeper <gabriel.saganic@keepersolutions.com>
2022-02-07 15:50:52 +0100

    added created recipe features

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -63,9 +63,15 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     def get_serializer_class(self):
         """
-        Return appropriate serializer class        
+        Return appropriate serializer class
         """
         if self.action == 'retrieve':
             return serializers.RecipeDetailSerializer
 
         return self.serializer_class
+
+    def perform_create(self, serializer):
+        """
+        Create a new recipe
+        """
+        serializer.save(user=self.request.user)

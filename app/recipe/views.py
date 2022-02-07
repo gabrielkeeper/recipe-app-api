599ad1e637fa33ac55811d1f99c6dfc0c915020a
gabrielkeeper <gabriel.saganic@keepersolutions.com>
2022-02-07 15:18:54 +0100

    add recipe detail view

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -60,3 +60,12 @@ class RecipeViewSet(viewsets.ModelViewSet):
         Retrive the recipes for the authenticed user
         """
         return self.queryset.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        """
+        Return appropriate serializer class        
+        """
+        if self.action == 'retrieve':
+            return serializers.RecipeDetailSerializer
+
+        return self.serializer_class
